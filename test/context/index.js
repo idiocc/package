cd72657db03244d2e6a931749ae8aa12cda50e83
Anton <anton@adc.sh>
2019-12-17 22:04:01 +0300

    test suite type

--- test/context/index.js
+++ test/context/index.js
@@ -34,6 +34,14 @@ export default class Context {
   }
 }
 
+/** @typedef {Object<string, Test & TestSuite4>} TestSuite */
+/** @typedef {Object<string, Test & TestSuite3>} TestSuite4 */
+/** @typedef {Object<string, Test & TestSuite2>} TestSuite3 */
+/** @typedef {Object<string, Test & TestSuite1>} TestSuite2 */
+/** @typedef {Object<string, Test & TestSuite0>} TestSuite1 */
+/** @typedef {Object<string, Test>} TestSuite0 */
+/** @typedef {(c: Context)} Test */
+
 let BIN = 'src/BIN'
 if (process.env.ALAMODE_ENV == 'test-build') {
   console.log('Testing build bin...')
